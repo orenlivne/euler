67c356157a27293950d4f3fbaa4aca1f4c584512
Oren Livne <oren.livne@gmail.com>
2015-03-18 19:35:45 -0500

    yoni organization

--- yoni/saw_the_stick.py
+++ yoni/saw_the_stick.py
@@ -22,56 +22,56 @@ Created on Mar 15, 2015
 import math, random
 
 def consecutive_triangular_sum(n, m):
-  # Returns the sum of the triangular numbers T_n,...,T_{m-1} where
-  # T_n = n*(n+1)/2, T_0 = 0.
-  return ((m - 1) * m * (m + 1) - (n - 1) * n * (n + 1)) / 6
+    # Returns the sum of the triangular numbers T_n,...,T_{m-1} where
+    # T_n = n*(n+1)/2, T_0 = 0.
+    return ((m - 1) * m * (m + 1) - (n - 1) * n * (n + 1)) / 6
 
 def triangular_sequence(n, m):
-  # Returns the sequence T_n,...,T_{m-1} of consecutive triangular numbers.
-  return [i * (i + 1) / 2 for i in xrange(n, m)]
+    # Returns the sequence T_n,...,T_{m-1} of consecutive triangular numbers.
+    return [i * (i + 1) / 2 for i in xrange(n, m)]
 
 def saw_the_stick(stick_len):
-  # Returns a longest list of consecutive triangular numbers whose sum is
-  # stick_len, if exists, otherwise returns an empty list.
-  # Complexity: O(N**0.5) using brute force over m and determining if there
-  # exists an n for each m.
-  m_max = int(math.ceil((2 * stick_len) ** (1. / 2.))) + 1
-  Sn_to_n_map = dict([((n - 1) * n * (n + 1) / 6, n) for n in xrange(1, m_max)])
-  solutions = filter(lambda (n, m): consecutive_triangular_sum(n, m) == stick_len,
-                     ((Sn_to_n_map[(m - 1) * m * (m + 1)/6 - stick_len], m)
-                      for m in xrange(1, m_max)
-                      if (m - 1) * m * (m + 1)/6 - stick_len in Sn_to_n_map))
-  return triangular_sequence(*max((m - n, (n, m)) for n, m in solutions)[1]) if solutions else []
+    # Returns a longest list of consecutive triangular numbers whose sum is
+    # stick_len, if exists, otherwise returns an empty list.
+    # Complexity: O(N**0.5) using brute force over m and determining if there
+    # exists an n for each m.
+    m_max = int(math.ceil((2 * stick_len) ** (1. / 2.))) + 1
+    Sn_to_n_map = dict([((n - 1) * n * (n + 1) / 6, n) for n in xrange(1, m_max)])
+    solutions = filter(lambda (n, m): consecutive_triangular_sum(n, m) == stick_len,
+                       ((Sn_to_n_map[(m - 1) * m * (m + 1)/6 - stick_len], m)
+                        for m in xrange(1, m_max)
+                        if (m - 1) * m * (m + 1)/6 - stick_len in Sn_to_n_map))
+    return triangular_sequence(*max((m - n, (n, m)) for n, m in solutions)[1]) if solutions else []
 
 def is_integer(n):
-  # Returns true if and only if n is an integer.
-  return abs(n - int(n)) < 1e-12
+    # Returns true if and only if n is an integer.
+    return abs(n - int(n)) < 1e-12
 
 def triangular_index(t):
-  # Converts a triangular number t = T_n to its index n. n is integer if and
-  # only if t is a triangular number.
-  return 0.5 * (-1 + (1 + 8 * t) ** 0.5)
+    # Converts a triangular number t = T_n to its index n. n is integer if and
+    # only if t is a triangular number.
+    return 0.5 * (-1 + (1 + 8 * t) ** 0.5)
 
 if __name__ == '__main__':
-  # These "asserts" using only for self-checking and not necessary for
-  # auto-testing.
-  assert saw_the_stick(225) == [105, 120], "1st example"
-  assert saw_the_stick(64) == [15, 21, 28], "1st example"
-  assert saw_the_stick(371) == [36, 45, 55, 66, 78, 91], "1st example"
-  assert saw_the_stick(882) == [], "1st example"
+    # These "asserts" using only for self-checking and not necessary for
+    # auto-testing.
+    assert saw_the_stick(225) == [105, 120], "1st example"
+    assert saw_the_stick(64) == [15, 21, 28], "1st example"
+    assert saw_the_stick(371) == [36, 45, 55, 66, 78, 91], "1st example"
+    assert saw_the_stick(882) == [], "1st example"
 
-  # Random tests.
-  for _ in xrange(1000):
-    m = random.randint(100, 2000)
-    n = random.randint(100, m)
-    N = consecutive_triangular_sum(n, m)
-    actual = saw_the_stick(N)
-    expected = triangular_sequence(n, m)
-    if actual != expected:
-      assert sum(actual) == N
-      assert len(actual) >= len(expected)
-      n_actual = triangular_index(actual[0])
-      assert is_integer(n_actual)
-      m_actual = triangular_index(actual[-1])
-      assert is_integer(m_actual)
-      assert actual, triangular_sequence(int(n_actual), int(m_actual))
+    # Random tests.
+    for _ in xrange(1000):
+        m = random.randint(100, 2000)
+        n = random.randint(100, m)
+        N = consecutive_triangular_sum(n, m)
+        actual = saw_the_stick(N)
+        expected = triangular_sequence(n, m)
+        if actual != expected:
+            assert sum(actual) == N
+            assert len(actual) >= len(expected)
+            n_actual = triangular_index(actual[0])
+            assert is_integer(n_actual)
+            m_actual = triangular_index(actual[-1])
+            assert is_integer(m_actual)
+            assert actual, triangular_sequence(int(n_actual), int(m_actual))
