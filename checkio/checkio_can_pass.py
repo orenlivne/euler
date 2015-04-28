641058457c4d2f31b431bbfd7dfc4a58c98f2345
Oren Livne <oren.livne@gmail.com>
2015-04-28 14:03:13 -0500

    solved djikstras

--- checkio/checkio_can_pass.py
+++ checkio/checkio_can_pass.py
@@ -3,10 +3,14 @@ import itertools as it, heapq
 infinity = float("inf")
 
 class Graph(object):
-  def __init__(self, edge_list):
+  def __init__(self, node_list, edge_list):
     self.nbhrs = dict()
+    for node in node_list: self.add_node(node)
     for edge in edge_list: self.add_edge(edge[0], edge[1], edge[2])
 
+  def add_node(self, u):
+    self.nbhrs.setdefault(u, {})
+    
   def add_edge(self, u, v, weight):
     self.nbhrs.setdefault(u, {}).__setitem__(v, weight)
     self.nbhrs.setdefault(v, {}).__setitem__(u, weight)
@@ -42,8 +46,8 @@ class PriorityQueue(object):
     entry = self.entry_finder.pop(task)
     entry[-1] = PriorityQueue.REMOVED
 
-  def __nonzero__(self):
-    return True if self.pq else False
+  def __len__(self):
+    return sum(1 for _, _, task in self.pq if task != PriorityQueue.REMOVED)
 
   def pop(self):
     # Remove and return the lowest priority task. Raise KeyError if empty.
@@ -65,14 +69,11 @@ def shortest_path_distance(g, source):
   Q = PriorityQueue((v, dist[v]) for v in g) # Unvisited nodes.
 
   while Q:
-    #print 'Q', Q.pq
     # Unvisited node with minimum distance from source. Initially, the source node.
     u, dist_u = Q.pop()
-    print 'u', u, 'dist_u', dist_u, 'neighbors', g.nbhrs[u].keys()
     for v, weight in g.nbhrs[u].iteritems():  # where v is still in Q.
       alt = dist_u + weight
       if alt < dist[v]:               # A shorter path to v has been found.
-        print 'Updating distance of v', v, 'from', dist[v], 'to', alt
         dist[v] = alt
         Q.add(v, alt)
         prev[v] = u
@@ -80,34 +81,36 @@ def shortest_path_distance(g, source):
 
 def can_pass(a, first, second):
   m, n = len(a), len(a[0])
-  g = Graph((u, v, 1) for u, v in
-            it.ifilter(lambda ((i, j), (k, l)): a[i][j] == a[k][l],
-                       it.chain(
-                           (((i, j), (i - 1, j)) for i in xrange(1, m) for j in xrange(n)),
-                           (((i, j), (i + 1, j)) for i in xrange(m - 1) for j in xrange(n)),
-                           (((i, j), (i, j - 1)) for i in xrange(m) for j in xrange(1, n)),
-                           (((i, j), (i, j + 1)) for i in xrange(m) for j in xrange(n - 1))
-                           )
-                     ))
-  dist, prev = shortest_path_distance(g, first)
-  print dist
+  g = Graph(((i, j) for i in xrange(m) for j in xrange(n)),
+            (((u, v, 1) for u, v in
+              it.ifilter(lambda ((i, j), (k, l)): a[i][j] == a[k][l],
+                         it.chain(
+                             (((i, j), (i - 1, j)) for i in xrange(1, m) for j in xrange(n)),
+                             (((i, j), (i + 1, j)) for i in xrange(m - 1) for j in xrange(n)),
+                             (((i, j), (i, j - 1)) for i in xrange(m) for j in xrange(1, n)),
+                             (((i, j), (i, j + 1)) for i in xrange(m) for j in xrange(n - 1))
+                             )
+                       )
+            )))
+  dist, _ = shortest_path_distance(g, first)
   return dist[second] != infinity
 
 
 if __name__ == '__main__':
-    assert can_pass(((0, 0, 0, 0, 0, 0),
-                     (0, 2, 2, 2, 3, 2),
-                     (0, 2, 0, 0, 0, 2),
-                     (0, 2, 0, 2, 0, 2),
-                     (0, 2, 2, 2, 0, 2),
-                     (0, 0, 0, 0, 0, 2),
-                     (2, 2, 2, 2, 2, 2),),
-                    (3, 2), (0, 5)) == True, 'First example'
-    assert can_pass(((0, 0, 0, 0, 0, 0),
-                     (0, 2, 2, 2, 3, 2),
-                     (0, 2, 0, 0, 0, 2),
-                     (0, 2, 0, 2, 0, 2),
-                     (0, 2, 2, 2, 0, 2),
-                     (0, 0, 0, 0, 0, 2),
-                     (2, 2, 2, 2, 2, 2),),
-                    (3, 3), (6, 0)) == False, 'First example'
+  assert can_pass(((1,9),(9,1)), (0,1), (1,0)) == False, '2x2 checkerboard'
+  assert can_pass(((0, 0, 0, 0, 0, 0),
+                   (0, 2, 2, 2, 3, 2),
+                   (0, 2, 0, 0, 0, 2),
+                   (0, 2, 0, 2, 0, 2),
+                   (0, 2, 2, 2, 0, 2),
+                   (0, 0, 0, 0, 0, 2),
+                   (2, 2, 2, 2, 2, 2),),
+                  (3, 2), (0, 5)) == True, 'First example'
+assert can_pass(((0, 0, 0, 0, 0, 0),
+                 (0, 2, 2, 2, 3, 2),
+                 (0, 2, 0, 0, 0, 2),
+                 (0, 2, 0, 2, 0, 2),
+                 (0, 2, 2, 2, 0, 2),
+                 (0, 0, 0, 0, 0, 2),
+                 (2, 2, 2, 2, 2, 2),),
+                (3, 3), (6, 0)) == False, 'First example'
