28c64cf8f1ae7c6e755c6df4b13e8da79fad2339
Oren Livne <oren.livne@gmail.com>
2015-04-28 13:52:28 -0500

    working on dijkstra impl

---
+++ checkio/checkio_can_pass.py
@@ -0,0 +1,113 @@
+import itertools as it, heapq
+
+infinity = float("inf")
+
+class Graph(object):
+  def __init__(self, edge_list):
+    self.nbhrs = dict()
+    for edge in edge_list: self.add_edge(edge[0], edge[1], edge[2])
+
+  def add_edge(self, u, v, weight):
+    self.nbhrs.setdefault(u, {}).__setitem__(v, weight)
+    self.nbhrs.setdefault(v, {}).__setitem__(u, weight)
+
+  def number_of_edges(self):
+    return sum(len(nbhrs) for nbhrs in self.nbhrs.itervalues())/2
+
+  def __iter__(self):
+    return self.nbhrs.__iter__()
+
+  def __str__(self):
+    return 'Graph[' + ','.join(map(str, ((u,v) for u in self.nbhrs for v in self.nbhrs[u]))) + ']'
+
+class PriorityQueue(object):
+  REMOVED = -100000      # placeholder for a removed task.
+
+  def __init__(self, tasks_and_priorities=[]):
+    self.pq = []                         # list of entries arranged in a heap
+    self.entry_finder = {}               # mapping of tasks to entries
+    self.counter = it.count()            # unique sequence count
+    for task, priority in tasks_and_priorities: self.add(task, priority)
+
+  def add(self, task, priority):
+    # Add a new task or update the priority of an existing task.
+    if task in self.entry_finder: self.remove(task)
+    self.count = next(self.counter)
+    entry = [priority, self.count, task]
+    self.entry_finder[task] = entry
+    heapq.heappush(self.pq, entry)
+
+  def remove(self, task):
+    # Mark an existing task as REMOVED.  Raise KeyError if not found.
+    entry = self.entry_finder.pop(task)
+    entry[-1] = PriorityQueue.REMOVED
+
+  def __nonzero__(self):
+    return True if self.pq else False
+
+  def pop(self):
+    # Remove and return the lowest priority task. Raise KeyError if empty.
+    while self.pq:
+      priority, count, task = heapq.heappop(self.pq)
+      if task != PriorityQueue.REMOVED:
+        del self.entry_finder[task]
+        return task, priority
+    raise KeyError('pop from an empty priority queue')
+
+def shortest_path_distance(g, source):
+  # Returns the shortest path distance from source to any node in g and
+  # a dictionary prev with a pointers to the "next-hop" node on the source graph
+  # to get the shortest route to the source.
+
+  # Initialize distances and previous nodes. All nodes initially in Q (unvisited nodes).
+  dist = dict((v, 0 if v == source else infinity) for v in g)
+  prev = dict((v, None) for v in g)
+  Q = PriorityQueue((v, dist[v]) for v in g) # Unvisited nodes.
+
+  while Q:
+    #print 'Q', Q.pq
+    # Unvisited node with minimum distance from source. Initially, the source node.
+    u, dist_u = Q.pop()
+    print 'u', u, 'dist_u', dist_u, 'neighbors', g.nbhrs[u].keys()
+    for v, weight in g.nbhrs[u].iteritems():  # where v is still in Q.
+      alt = dist_u + weight
+      if alt < dist[v]:               # A shorter path to v has been found.
+        print 'Updating distance of v', v, 'from', dist[v], 'to', alt
+        dist[v] = alt
+        Q.add(v, alt)
+        prev[v] = u
+  return dist, prev
+
+def can_pass(a, first, second):
+  m, n = len(a), len(a[0])
+  g = Graph((u, v, 1) for u, v in
+            it.ifilter(lambda ((i, j), (k, l)): a[i][j] == a[k][l],
+                       it.chain(
+                           (((i, j), (i - 1, j)) for i in xrange(1, m) for j in xrange(n)),
+                           (((i, j), (i + 1, j)) for i in xrange(m - 1) for j in xrange(n)),
+                           (((i, j), (i, j - 1)) for i in xrange(m) for j in xrange(1, n)),
+                           (((i, j), (i, j + 1)) for i in xrange(m) for j in xrange(n - 1))
+                           )
+                     ))
+  dist, prev = shortest_path_distance(g, first)
+  print dist
+  return dist[second] != infinity
+
+
+if __name__ == '__main__':
+    assert can_pass(((0, 0, 0, 0, 0, 0),
+                     (0, 2, 2, 2, 3, 2),
+                     (0, 2, 0, 0, 0, 2),
+                     (0, 2, 0, 2, 0, 2),
+                     (0, 2, 2, 2, 0, 2),
+                     (0, 0, 0, 0, 0, 2),
+                     (2, 2, 2, 2, 2, 2),),
+                    (3, 2), (0, 5)) == True, 'First example'
+    assert can_pass(((0, 0, 0, 0, 0, 0),
+                     (0, 2, 2, 2, 3, 2),
+                     (0, 2, 0, 0, 0, 2),
+                     (0, 2, 0, 2, 0, 2),
+                     (0, 2, 2, 2, 0, 2),
+                     (0, 0, 0, 0, 0, 2),
+                     (2, 2, 2, 2, 2, 2),),
+                    (3, 3), (6, 0)) == False, 'First example'
--- checkio/checkio_digits_doublets.py
+++ checkio/checkio_digits_doublets.py
@@ -1,17 +1,17 @@
 class Graph(object):
-      def __init__(self, edge_list):
-            self.nbhrs = dict()
-            for edge in edge_list: self.add_edge(edge[0], edge[1])
+  def __init__(self, edge_list):
+    self.nbhrs = dict()
+    for edge in edge_list: self.add_edge(edge[0], edge[1])
 
-      def add_edge(self, u, v):
-            self.nbhrs.setdefault(u, set([])).add(v)
-            self.nbhrs.setdefault(v, set([])).add(u)
-      
-      def __repr__(self):
-            print 'Graph edges:'
-            for u in self.nbhrs:
-                  for v in self.nbhrs[u]:
-                        print u, v
+    def add_edge(self, u, v):
+      self.nbhrs.setdefault(u, set([])).add(v)
+      self.nbhrs.setdefault(v, set([])).add(u)
+
+    def __repr__(self):
+      print 'Graph edges:'
+      for u in self.nbhrs:
+        for v in self.nbhrs[u]:
+          print u, v
 
 if __name__ == '__main__':
 #      t = [('121', '123'), ('123', '193'), ('193', '993'), ('121', '125')
