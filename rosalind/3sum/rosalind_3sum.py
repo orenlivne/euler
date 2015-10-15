133ee435ef40ac41a76505c8c0b97943eec522b3
Oren Livne <oren.livne@gmail.com>
2015-10-15 14:16:05 -0500

    Added n + L log L algorithm

--- rosalind/3sum/rosalind_3sum.py
+++ rosalind/3sum/rosalind_3sum.py
@@ -12,9 +12,64 @@ if exist, and "-1" otherwise.
 ============================================================
 '''
 import rosutil as ru, itertools as it, random, numpy as np
+from scipy import signal
 
-def two_sum_indices(a, s):
-  # Given an array a, outputs (p, q), indices such that 1 <= p < q <= n
+def three_sum_indices_fft(a, s):
+  # Given an array a of integers between L1 and L2, outputs (p, q), indices
+  # such that 1 <= p < q < r <= n such that A[p] + A[q] + A[r] = s if exist,
+  # and None otherwise. Builds a hash table of A[q] + A[r] values using FFT
+  # convolution of a bit vector representing A with itself, then proceeds like
+  # the 2SUM solver (loop over p, find (q,r) using the hash).
+  # Complexity: O(n + L log L) where L = L2 - L1.
+
+  # Build the bit vector of size L. b[0] corresponds to a_min, b[1] to whether
+  # or not a_min + 1 is in a, etc.
+  a_min, a_max, n = a[0], a[0], len(a)
+  for i in xrange(1, n):
+    x = a[i]
+    if x < a_min: a_min = x
+    if x > a_max: a_max = x
+  L = a_max - a_min + 1
+  b = [0] * L
+  for x in a: b[x - a_min] = 1
+
+  # Multiply b with itself using FFT convolution. The locations of 1-bits in
+  # the product correspond to all sums y + z of element pairs y, z in a. Store
+  # all sums in a set so we can look a sum up in O(1).
+  offset = 2 * a_min
+  a_2sum = set(i + offset for i, x in enumerate(signal.fftconvolve(b, b)) if abs(x) > 1e-12)
+
+  # Build a hash table of element values to indices. An element may be repeated,
+  # so a value is mapped to a set of indices.
+  d = {}
+  for i, x in enumerate(a):
+    d.setdefault(x, set()).add(i)
+
+  # For each p, find whether there exists a sum t = s - A[p] = A[q] + A[r] in
+  # a_2sum.
+  for p, x in ((p, x) for p, x in enumerate(a) if (s - x) in a_2sum):
+    t = s - x
+    # Sum exists, loop over q's and find one for which there exists an r
+    # such that A[q] + A[r] = t.
+    for q, y in enumerate(a[p + 1:], p + 1):
+      z = t - y
+      if d.has_key(z):
+        indices = d[z]
+        if y == z and len(indices) > 1:
+          # There exist two elements A[q] = A[r] = t/2.
+          for r in indices:
+            if r != q:
+              break
+          return (p, q, r)
+        else:
+          # There exist two differnt elements A[q], A[r] whose sum is t.
+          for r in indices:
+            if q < r:
+              return (p, q, r)
+  return None
+
+def two_sum_indices(a, s, d=None):
+  # Given an integer array a, outputs (p, q), indices such that 1 <= p < q <= n
   # such that A[p] + A[q] = s if exist, and None otherwise.
   # Complexity: O(n log n).
 
@@ -25,7 +80,8 @@ def two_sum_indices(a, s):
     if len(zero_elements) >= 2:
       return tuple(zero_elements[:2])
 
-  d = dict((x, i) for i, x in enumerate(a))
+  if d is not None:
+    d = dict((x, i) for i, x in enumerate(a))
   for p, x in enumerate(a):
     if d.has_key(s - x):
       q = d[s - x]
@@ -40,16 +96,20 @@ def three_sum_indices_2sum(a, s):
   # Given an array a, outputs (p, q), indices such that 1 <= p < q < r <= n
   # such that A[p] + A[q] + A[r] = s if exist, and None otherwise. Uses n
   # calls of the 2SUM solver (loop over p, find (q,r) using 2SUM).
-  # Complexity: O(n^2 log n).
+  # Complexity: O(n^2).
+  # This dictionary can be cached instead of re-created n times inside
+  # two_sum_indices (this however doesn't change the overall complexity).
+  d = dict((x, i) for i, x in enumerate(a))
   for p, x in enumerate(a):
-    remaining_indices = two_sum_indices(a[p+1:], s - x)
+    remaining_indices = two_sum_indices(a[p+1:], s - x, d)
     if remaining_indices is not None:
       # Map remaining_indices from the sub-array a[p+1:] back to indices in a.
       return (p, ) + tuple(map(lambda x: x + p + 1, remaining_indices))
   return None
 
 # Alias for the best algorithm.
-three_sum_indices_fast = three_sum_indices_2sum
+three_sum_indices_fast = three_sum_indices_fft
+#three_sum_indices_fast = three_sum_indices_2sum
 
 # A value indicating that a solution was not found.
 NOT_FOUND = -1
@@ -112,6 +172,6 @@ def test_vs_brute_force_from_file(file_name):
 
 if __name__ == "__main__":
 #  test_vs_brute_force_from_file('rosalind_3sum.dat')
-  test_vs_brute_force_random()
-  print three_sum('rosalind_3sum_sample.dat')
-#  print three_sum('rosalind_3sum.dat')
+#  print three_sum('rosalind_3sum_sample.dat')
+#  test_vs_brute_force_random()
+  print three_sum('rosalind_3sum.dat')
