397aa694ab5e6e1d294ebbdb7f813e35d1716b91
Oren Livne <oren.livne@gmail.com>
2015-10-25 12:42:03 -0500

    d

--- yoni/multi_birthday.py
+++ yoni/multi_birthday.py
@@ -7,15 +7,16 @@ Created on Oct 25, 2015
 @author: Oren Livne <livne@uchicago.edu>
 ============================================================
 '''
-import numpy as np, matplotlib.pyplot as P, scipy.special
+import numpy as np, matplotlib.pyplot as P, itertools as it
 from collections import Counter
 
-NUM_DAYS = 365  # Ignoring February 29
+NUM_SOLAR_DAYS = 365  # Ignoring February 29
 
 def no_pair_probability(n, k):
     '''Returns the probability that no two people have the same birthday among
     a group of n people. k is the number of days in a year (i.e., number of
     birthday possibilities).'''
+    # Calculating the log of the probability first to retain numerical accuracy.
     return np.exp(sum(np.log(k - i) for i in xrange(n)) - n * np.log(k))
 
 def pair_probability(n, k):
@@ -24,13 +25,19 @@ def pair_probability(n, k):
     birthday possibilities).'''
     return 1 - no_pair_probability(n, k)
 
+def num_configs_with_i_pairs(n, k, i):
+    '''Returns the number of configurations of n birthdays with k equal pairs and
+    n - 2*k singletons.''' 
+    # Calculating the log of the probability first to retain numerical accuracy.
+    return np.exp(sum(np.log(n - j) for j in xrange(2 * i)) 
+                  - sum(np.log(j) for j in xrange(2, i + 1))
+                  - i * np.log(2 * k))
+
 def triplet_probability(n, k):
     '''Returns the probability that there exist 3 people with the same birthday
     among a group of n people. k is the number of days in a year (i.e., number of
     birthday possibilities).'''
-    for i in xrange(n / 2):
-        print i, 'pairs', 'p', no_pair_probability(n - 2 * i, k), scipy.special.binom(n, i) / (k ** i), scipy.special.binom(n, i)
-    return 1 - sum((scipy.special.binom(n, i) / (k ** i)) * no_pair_probability(n - 2 * i, k) for i in xrange(n / 2))  
+    return 1 - sum(num_configs_with_i_pairs(n, k, i) * no_pair_probability(n - i, k) for i in xrange(n / 2 + 1))  
       
 def multi_birthday_probability(n, m, k):
     '''Returns the probability that there exist m people with
@@ -50,27 +57,35 @@ def multi_birthday_probability_experimental(n, m, k, sample_size=100):
     (i.e., number of birthday possibilities) using random sampling.'''
     return float(sum(multi_birthday_probability_of_sample(n, m, k) for _ in xrange(sample_size))) / sample_size
 
-def plot_probability(m):
-    N = np.arange(1, 50)
-    P.plot(N, [multi_birthday_probability(n, m, NUM_DAYS) for n in N], 'b-', \
-           N, [multi_birthday_probability_experimental(n, m, NUM_DAYS, sample_size=100) for n in N], 'g-', \
-           N, [multi_birthday_probability_experimental(n, m, NUM_DAYS, sample_size=10000) for n in N], 'r-')
+def plot_probability(k, sample_size=10000):
+    N = np.arange(1, 200)
+    p_pair = [multi_birthday_probability(n, 2, k) for n in N]
+    p_pair_experimental = [multi_birthday_probability_experimental(n, 2, k, sample_size=sample_size) for n in N]
+    p_triple = [multi_birthday_probability(n, 3, k) for n in N]
+    p_triple_experimental = [multi_birthday_probability_experimental(n, 3, k, sample_size=sample_size) for n in N]
+    # Find the smallest n such that p >= 0.5. Assuming p is monotonically increasing with N.
+    N_half_pair = it.dropwhile(lambda (n, p): p < 0.5, zip(N, p_pair)).next()[0]
+    N_triple_pair = it.dropwhile(lambda (n, p): p < 0.5, zip(N, p_triple)).next()[0]
+    
+    P.plot(N, p_pair, 'b-', N, p_pair_experimental, 'b--',
+           N, p_triple, 'r-', N, p_triple_experimental, 'r--')
+    P.legend(['m = 2, theoretical', 'm = 2, %d samples' % (sample_size,),
+              'm = 3, theoretical', 'm = 3, %d samples' % (sample_size,)],
+             loc='lower right')
     P.grid(True)
-    P.legend(['Theoretical', '100 Samples', '10000 samples', '40000 samples'], loc='upper left')
     P.xlabel('# People')
     P.ylabel('Probability')
-    P.title('Probability of %d people with the same birthday' % (m,))
+    P.title('Probability of m people with the same birthday')
+    y_limits = (-0.01, 1.01)
+    P.plot([N_half_pair, N_half_pair], y_limits, 'k--')
+    P.plot([N_triple_pair, N_triple_pair], y_limits, 'k--')
+    P.ylim(y_limits)
     P.show()
     
-def compare_probabilitie(n, m):
-    print 'Theoretical', n, m, multi_birthday_probability(n, m, NUM_DAYS)
-    print 'Experimental'
-    for N in 2 ** np.arange(15):
-        print N, multi_birthday_probability_experimental(n, m, NUM_DAYS, sample_size=N)
+def compare_probabilities(n, m, sample_size=2 ** 16):
+    print 'Theoretical', n, m, 100. * multi_birthday_probability(n, m, NUM_SOLAR_DAYS), '%'
+    print 'Experimental(sample size %d)' % (sample_size,), 100. * multi_birthday_probability_experimental(n, m, NUM_SOLAR_DAYS, sample_size=sample_size), '%'
 
 if __name__ == '__main__':
-    # compare_probabilitie(20, 2)
-    compare_probabilitie(20, 3)
-    # TODO: show all probabilities on the same graph
-#    plot_probability(2)
-    # plot_probability(3)
+    compare_probabilities(20, 3)  # Oberlab / Caliskan+Chong+Igartua 
+    plot_probability(NUM_SOLAR_DAYS)
